d460f9b33dbc6bfe05396888ed2e18c1a40345f3
Vincent <cloudy.sonata@gmail.com>
2016-08-16 14:30:11 -0400

    Make button insert into database

--- app/src/main/java/frostlight/fluffyshoppinglist/MainActivity.java
+++ app/src/main/java/frostlight/fluffyshoppinglist/MainActivity.java
@@ -1,5 +1,6 @@
 package frostlight.fluffyshoppinglist;
 
+import android.content.ContentValues;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import frostlight.fluffyshoppinglist.data.DbContract;
 
 public class MainActivity extends AppCompatActivity {
+    Button mAddButton;          // Button to add groceries with
+    EditText mGroceryItemEdit;  // Name of the grocery
+    EditText mGroceryUnitEdit;  // Unit of the grocery (optional)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +26,32 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Bind UI elements
+        mAddButton = (Button)findViewById(R.id.groceryAddButton);
+        mGroceryItemEdit = (EditText)findViewById(R.id.groceryItemEdit);
+        mGroceryUnitEdit = (EditText)findViewById(R.id.groceryUnitEdit);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        // Add button should insert grocery into databases
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Insert each element into the database
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(DbContract.GroceryEntry.COLUMN_NAME, mGroceryItemEdit.getText().toString());
+                contentValues.put(DbContract.GroceryEntry.COLUMN_QUANTITY, mGroceryUnitEdit.getText().toString());
+
+                // Placeholder ID
+                contentValues.put(DbContract.GroceryEntry.COLUMN_SHOPPINGLISTID, "0");
             }
         });
     }
